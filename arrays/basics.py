585c716afc29ef63fb2c68d876a5de599fc16190
consistentCoder-s01 <sinchana.cs18@gmail.com>
2026-03-22 01:33:28 +0530

    Added array basics.py with operations

---
+++ arrays/basics.py
@@ -0,0 +1,22 @@
+#create an array
+arr=[10,20,30]
+
+#Access
+print(arr[0])
+
+#Slicing
+print(arr[1:3])
+
+#Add
+arr.append(40)
+
+#Update
+arr[1]=25
+
+#Remove
+arr.remove(30)
+
+#Pop
+arr.pop()
+
+print(arr)
