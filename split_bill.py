c58f3aefa59a2767fcbb5b83fe26932d1bddbab6
consistentCoder-s01 <sinchana.cs18@gmail.com>
2026-03-26 21:31:24 +0530

    Added split bill among friends program

---
+++ split_bill.py
@@ -0,0 +1,6 @@
+#Split restaurant bill among friends
+
+total_bill=float(input("Enter total bill:"))
+friends=int(input("Enter number of friends:"))
+share= toatal_bill/friends
+print("Each person should pay:",share)
